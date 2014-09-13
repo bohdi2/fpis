9cc9bc8918e5ce66c1b825e376cdbc77e0e3eff9
Christopher Helck <christopher.helck@gmail.com>
2014-09-13 14:34:55 -0400

    Initial Files

---
+++ src/main/scala/Chapter2.scala
@@ -0,0 +1,43 @@
+
+object Chapter2 {
+
+  // Exercise 2.2
+
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean) = {
+
+    def loop(n: Int): Boolean = {
+      if (n >= (as.length - 1)) true
+      else if (! ordered(as(n), as(n+1))) false
+      else loop(n+1)
+    }
+    loop(0)
+  }
+
+  // Exercise 2.3
+
+  def curry1[A,B,C](f: (A,B) => C): A => (B => C) = {
+    def x(a: A) = {
+      def y(b: B) = f(a,b)
+      y _
+    }
+    x
+  }
+
+  def curry2[A,B,C](f: (A,B) => C): A => (B => C) = {
+    (a: A) => { (b: B) => f(a,b) }
+  }
+
+  def curry3[A,B,C](f: (A,B) => C): A => (B => C) = {
+    (a: A) => (b: B) => f(a,b)
+  }
+
+  def curry4[A,B,C](f: (A,B) => C): A => (B => C) = {
+    a => b => f(a,b)
+  }
+
+  def curry[A,B,C](f: (A,B) => C): A => B => C = {
+    x => y => f(x,y)
+  }
+
+
+}
